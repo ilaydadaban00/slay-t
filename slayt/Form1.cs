532eb69d1d523160ec74fb66000967d91082ace1
İlayda <145451273+ilaydadaban00@users.noreply.github.com>
2024-01-16 10:15:00 +0300

    Add files via upload

---
+++ slayt/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace slayt
+{
+    public partial class Form1 : Form
+    {
+        BindingList<Bitmap>resimler=new BindingList<Bitmap>();
+        int sayac=0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            resimler.Add(Properties.Resources.indir);
+            resimler.Add(Properties.Resources.indir__1_);
+            //   resimler.Add(Properties.Resources.resim3);
+            //  resimler.Add(Properties.Resources.resim4);
+            // resimler.Add(Properties.Resources.resim5);
+            timer1.Start();
+        }
+
+       
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            sayac %=resimler.Count;
+            pictureBox1.Image= resimler[sayac];
+        }
+    }
+}
